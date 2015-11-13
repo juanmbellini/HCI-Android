e0acd30cf791ba725ec7c62d052f3f4d7ecbf974
jmb <juanmbellini@hotmail.com>
2015-11-13 14:11:49 -0300

    HomeToolBar

--- TiendApp/app/src/main/java/hci/tiendapp/HomeActivity.java
+++ TiendApp/app/src/main/java/hci/tiendapp/HomeActivity.java
@@ -1,13 +1,27 @@
 package hci.tiendapp;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
     }
 }
