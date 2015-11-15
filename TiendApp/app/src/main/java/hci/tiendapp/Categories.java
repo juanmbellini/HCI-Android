10527319603b116808f30efd43762fdaf54e19a5
safir93 <safir_93@live.se>
2015-11-14 23:08:29 -0300

    Icons, menus, activities
    Not as much as it seems.

--- TiendApp/app/src/main/java/hci/tiendapp/Categories.java
+++ TiendApp/app/src/main/java/hci/tiendapp/Categories.java
@@ -1,24 +1,16 @@
 package hci.tiendapp;
 
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 
-public class HomeActivity extends AppCompatActivity {
-
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle drawerToggle;
+public class Categories extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-
+        setContentView(R.layout.activity_categories);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
