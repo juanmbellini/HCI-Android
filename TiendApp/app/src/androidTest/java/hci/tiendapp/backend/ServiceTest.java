09c6642ec69c6f8f9afad14d1b863a4d372f3633
julianrod94 <julrodriguez@itba.edu.ar>
2015-11-18 17:32:38 -0300

    service andando

---
+++ TiendApp/app/src/androidTest/java/hci/tiendapp/backend/ServiceTest.java
@@ -0,0 +1,18 @@
+package hci.tiendapp.backend;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Julian on 11/17/2015.
+ */
+public class ServiceTest extends TestCase {
+
+
+    Service service = new Service();
+
+
+    public void testRequest() throws Exception {
+        Service service = new Service();
+        service.connect("http://eiffel.itba.edu.ar/hci/service3/Common.groovy?method=GetAllStates");
+    }
+}
--- TiendApp/app/src/main/java/hci/tiendapp/backend/Service.java
+++ TiendApp/app/src/main/java/hci/tiendapp/backend/Service.java
@@ -1,19 +1,98 @@
 package hci.tiendapp.backend;
 
 import android.app.Activity;
-import android.util.Log;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
-/**
- * Created by Julian on 11/17/2015.
- */
-public class Service extends Activity {
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
+public class Service{
 
+    JSONObject requestResult = new JSONObject();
+
+    /*
+     * To convert the InputStream to String we use the BufferedReader.readLine()
+     * method. We iterate until the BufferedReader return null which means
+     * there's no more data to read. Each line will appended to a StringBuilder
+     * and returned as String.
+     */
+
+    public Service(){
+
+    }
+
+    public void connect(String urlString){
+        URL url;
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return;
+        }
+        HttpURLConnection urlConnection;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            requestResult = new JSONObject(readStream(in));
+
+            urlConnection.disconnect();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readStream(InputStream is) {
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while(i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+
+    public static String streamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
 
 }
