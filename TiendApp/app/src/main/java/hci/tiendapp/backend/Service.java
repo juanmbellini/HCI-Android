d447cecdfa5d727ee3aaf14a2d4c0c7baf718ef2
julianrod94 <julrodriguez@itba.edu.ar>
2015-11-18 15:26:32 -0300

    service in progress

---
+++ TiendApp/app/src/main/java/hci/tiendapp/backend/Service.java
@@ -0,0 +1,19 @@
+package hci.tiendapp.backend;
+
+import android.app.Activity;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Julian on 11/17/2015.
+ */
+public class Service extends Activity {
+
+
+
+
+}
