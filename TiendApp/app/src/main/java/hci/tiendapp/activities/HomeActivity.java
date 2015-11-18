491a6b0cab83c7c0ce1615b60bb5d3760832b750
jmb <juanmbellini@hotmail.com>
2015-11-18 02:52:41 -0300

    NavigationDrawerNoCableado

--- TiendApp/app/src/main/java/hci/tiendapp/activities/HomeActivity.java
+++ TiendApp/app/src/main/java/hci/tiendapp/activities/HomeActivity.java
@@ -1,4 +1,4 @@
-package hci.tiendapp;
+package hci.tiendapp.activities;
 
 import android.content.res.Configuration;
 import android.support.v4.widget.DrawerLayout;
@@ -7,21 +7,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import hci.tiendapp.R;
+
+import static android.widget.Toast.LENGTH_LONG;
+
 public class HomeActivity extends AppCompatActivity {
 
-    private String[] mPlanetTitles = {"Home", "Hombres", "Mujeres", "Infantiles", "Bebes"};
-    private int[] icons = {R.drawable.ic_home, R.drawable.ic_men_category, R.drawable.ic_women_category, R.drawable.ic_kids_category, R.drawable.ic_babies_category};
-    String name = "Juan Marcos Bellini";
-    String email = "jbellini@itba.edu.ar";
-    int profile = R.drawable.logo2;
+    private Toolbar toolbar;
 
 
     private RecyclerView mDrawerList;
@@ -35,13 +36,21 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+/*
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.app_name);*/
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         mTitle = "test";
         drawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer_layout);
         mDrawerList = (RecyclerView) findViewById(R.id.left_drawer);
         mDrawerList.setHasFixedSize(true);
 
-        RecyclerView.Adapter mAdapter= new MyAdapter(mPlanetTitles, icons, name, email, profile);
+        RecyclerView.Adapter mAdapter= new MyAdapter();
 
         mDrawerList.setAdapter(mAdapter);//new ArrayAdapter<String>(this, R.layout.navigation_drawer_option, mPlanetTitles));
 
@@ -64,17 +73,13 @@ public class HomeActivity extends AppCompatActivity {
 
         drawerLayout.setDrawerListener(drawerToggle);
         drawerLayout.setFitsSystemWindows(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
 
-        //mDrawerList.addHeaderView(findViewById(R.id.drawer_header));
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.home_menu, menu);
-
         return true;
     }
 
@@ -95,8 +100,9 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
+        // true, then it has handled the app navigationDrawerOptionIcon touch event
         if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
@@ -113,15 +119,11 @@ public class HomeActivity extends AppCompatActivity {
 
         // Highlight the selected item, update the title, and close the drawer
         //mDrawerList.setItemChecked(position, true);
-        setTitle(mPlanetTitles[position]);
+        //setTitle(mPlanetTitles[position]);
         drawerLayout.closeDrawer(mDrawerList);
     }
 
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getSupportActionBar().setTitle(mTitle);
-    }
+
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
@@ -130,6 +132,15 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
+    // TODO ver si con esto funciona el custom tool bar
+    private class ToolBarItemClickListener implements android.support.v7.widget.Toolbar.OnMenuItemClickListener {
+
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            return false;
+        }
+    }
 
 
 }
---
+++ TiendApp/app/src/main/java/hci/tiendapp/activities/MyAdapter.java
@@ -0,0 +1,320 @@
+package hci.tiendapp.activities;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.MalformedInputException;
+import java.util.ArrayList;
+import java.util.List;
+
+import hci.tiendapp.R;
+import hci.tiendapp.TiendApp;
+
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private static final int TYPE_HEADER = 0;  // Declaring Variable to Understand which View is being worked on
+    // IF the view under inflation and population is header or Item
+    private static final int TYPE_ITEM = 1;
+
+
+
+    // Creating a ViewHolder which extends the RecyclerView View Holder
+    // ViewHolder are used to to store the inflated views in order to recycle them
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        int Holderid;
+
+        TextView textView;
+        ImageView imageView;
+        ImageView profile;
+        TextView Name;
+        TextView email;
+
+
+        public ViewHolder(View itemView,int ViewType) {                 // Creating ViewHolder Constructor with View and viewType As a parameter
+            super(itemView);
+
+
+            // Here we set the appropriate view in accordance with the the view type as passed when the holder object is created
+
+            if(ViewType == TYPE_ITEM) {
+                textView = (TextView) itemView.findViewById(R.id.drawer_option_name); // Creating TextView object with the id of textView from item_row.xml
+                imageView = (ImageView) itemView.findViewById(R.id.drawer_option_icon);// Creating ImageView object with the id of ImageView from item_row.xml
+                Holderid = 1;                                               // setting holder id as 1 as the object being populated are of type item row
+            }
+            else{
+
+
+                Name = (TextView) itemView.findViewById(R.id.drawer_name);         // Creating Text View object from header.xml for name
+                email = (TextView) itemView.findViewById(R.id.drawer_email);       // Creating Text View object from header.xml for email
+                profile = (ImageView) itemView.findViewById(R.id.user_image);// Creating Image view object from header.xml for profile pic
+                Holderid = 0;                                                // Setting holder id = 0 as the object being populated are of type header view
+            }
+        }
+
+
+    }
+
+
+
+    MyAdapter(){
+        super();
+
+    }
+
+
+
+    //Below first we ovverride the method onCreateViewHolder which is called when the ViewHolder is
+    //Created, In this method we inflate the item_row.xml layout if the viewType is Type_ITEM or else we inflate header.xml
+    // if the viewType is TYPE_HEADER
+    // and pass it to the view holder
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        if (viewType == TYPE_ITEM) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.navigation_drawer_option,parent,false); //Inflating the layout
+
+            ViewHolder vhItem = new ViewHolder(v,viewType); //Creating ViewHolder and passing the object of type view
+
+            return vhItem; // Returning the created object
+
+            //inflate your layout and pass it to view holder
+
+        } else if (viewType == TYPE_HEADER) {
+
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.navigation_drawer_header,parent,false); //Inflating the layout
+
+            ViewHolder vhHeader = new ViewHolder(v,viewType); //Creating ViewHolder and passing the object of type view
+
+            return vhHeader; //returning the object created
+
+
+        }
+        return null;
+
+    }
+
+    //Next we override a method which is called when the item in a row is needed to be displayed, here the int position
+    // Tells us item at which position is being constructed to be displayed and the holder id of the holder object tell us
+    // which view type is being created 1 for item row
+    @Override
+    public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
+        if(holder.Holderid ==1) {                              // as the list view is going to be called after the header view so we decrement the
+            // position by 1 and pass it to the holder while setting the text and image
+            //holder.textView.setText(mNavTitles[position - 1]); // Setting the Text with the array of our Titles
+            //holder.imageView.setImageResource(mIcons[position -1]);// Settimg the image with array of our icons
+
+            // Sets the option name
+            holder.textView.setText(NavigationDrawerOptions.getInstance().getValues()[position - 1].navigationDrawerOptionName);
+
+            if (NavigationDrawerOptions.getInstance().getValues()[position - 1].getClass() == NavigationDrawerOptions.NavigationDrawerMainOption.class) {
+                NavigationDrawerOptions.NavigationDrawerMainOption aux = null;
+                aux = (NavigationDrawerOptions.NavigationDrawerMainOption) NavigationDrawerOptions.getInstance().getValues()[position - 1];
+                holder.imageView.setImageResource(aux.navigationDrawerOptionIcon);
+            } else {
+                holder.imageView.setVisibility(View.GONE);
+            }
+
+        }
+        else{
+
+            holder.profile.setImageBitmap(NavigationDrawerHeader.getInstance().profile);
+            holder.Name.setText(NavigationDrawerHeader.getInstance().name);
+            holder.email.setText(NavigationDrawerHeader.getInstance().email);
+
+        }
+    }
+
+    // This method returns the number of items present in the list
+    @Override
+    public int getItemCount() {
+        return NavigationDrawerOptions.getInstance().getValues().length + 1; // the number of items in the list will be +1 the titles including the header view.
+    }
+
+
+    // Witht the following method we check what type of view is being passed
+    @Override
+    public int getItemViewType(int position) {
+        if (isPositionHeader(position))
+            return TYPE_HEADER;
+
+        return TYPE_ITEM;
+    }
+
+    private boolean isPositionHeader(int position) {
+        return position == 0;
+    }
+
+
+    public void setUserForHeader(String name, String email, String profilePic) {
+        NavigationDrawerHeader.setUser(name, email, profilePic);
+    }
+
+
+    private static class NavigationDrawerHeader {
+
+        private static NavigationDrawerHeader ourInstance = new NavigationDrawerHeader();
+
+        private String name;
+        private String email;
+        private Bitmap profile;
+
+        private NavigationDrawerHeader() {
+
+            name = "Juan Marcos Bellini";
+            email = "jbellini@itba.edu.ar";
+            profile = null;//R.drawable.logo2;
+        }
+
+        private NavigationDrawerHeader(String name, String email, Bitmap profile) {
+            this.name = name;
+            this.email = email;
+            this.profile = profile;
+        }
+
+        public static NavigationDrawerHeader getInstance() {
+            return ourInstance;
+        }
+
+        public static void setUser(String name, String email, String profilePic) {
+            Bitmap bitmap = null;
+            try {
+                bitmap = BitmapFactory.decodeStream((InputStream) new URL(profilePic).getContent());
+            } catch (MalformedInputException e) {
+                e.printStackTrace();
+                bitmap = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                bitmap = null;
+            } finally {
+                ourInstance = new NavigationDrawerHeader(name, email, bitmap);
+            }
+
+        }
+
+
+    }
+
+
+    /**
+     * Inner Class representing the navigation drawer options
+     */
+    private static class NavigationDrawerOptions {
+
+        /**
+         * Singleton
+         */
+        private static NavigationDrawerOptions ourInstance = new NavigationDrawerOptions();
+
+        // Stores options for Navigation Drawer
+        private List<NavigationDrawerOption> options;
+
+
+        /**
+         * Gets the singleton
+         *
+         * @return The only instance of the Navigation Drawer Options Class
+         */
+        public static NavigationDrawerOptions getInstance() {
+            return ourInstance;
+        }
+
+        // Private constructor
+        private NavigationDrawerOptions() {
+
+            options = new ArrayList<NavigationDrawerOption>();
+
+            fillOptions(options);
+
+        }
+
+
+        // Method to fill the list
+        private void fillOptions(List<NavigationDrawerOption> list) {
+
+            Integer currentOption = null;
+            Integer currentIcon = null;
+
+
+            // Now adding Home
+            currentOption = R.string.drawer_option_home;
+            currentIcon = R.drawable.ic_home;
+            list.add(new NavigationDrawerMainOption(1, currentOption, currentIcon));
+
+
+            // Now adding Men
+            currentOption = R.string.drawer_option_men;
+            currentIcon = R.drawable.ic_men_category;
+            list.add(new NavigationDrawerMainOption(2, currentOption, currentIcon));
+
+            // Now adding Women
+            currentOption = R.string.drawer_option_women;
+            currentIcon = R.drawable.ic_women_category;
+            list.add(new NavigationDrawerMainOption(3, currentOption, currentIcon));
+
+            // Now adding Kids
+            currentOption = R.string.drawer_option_kids;
+            currentIcon = R.drawable.ic_kids_category;
+            list.add(new NavigationDrawerMainOption(4, currentOption, currentIcon));
+
+            // Now adding Babies
+            currentOption = R.string.drawer_option_babies;
+            currentIcon = R.drawable.ic_babies_category;
+            list.add(new NavigationDrawerMainOption(5, currentOption, currentIcon));
+
+            // Now adding Orders
+            currentOption = R.string.drawer_option_orders;
+            currentIcon = R.drawable.ic_orders;
+            list.add(new NavigationDrawerMainOption(6, currentOption, currentIcon));
+        }
+
+        /**
+         * Gets the options as an array
+         *
+         * @return An array of NavigationDrawerOption that contains each navigationDrawerOptionName for the Navigation Drawer
+         */
+        public NavigationDrawerOption[] getValues() {
+
+            NavigationDrawerOption[] array = new NavigationDrawerOption[options.size()];
+            return options.toArray(array);
+        }
+
+        // Inner Class representing a Navigation Drawer Option
+        private static class NavigationDrawerOption {
+
+            int position;
+            int navigationDrawerOptionName;
+
+            public NavigationDrawerOption(int position, int navigationDrawerOptionName) {
+                this.position = position;
+                this.navigationDrawerOptionName = navigationDrawerOptionName;
+            }
+
+        }
+
+        // Inner Class representing a Navigation Drawer Option that contains an navigationDrawerOptionIcon
+        private static class NavigationDrawerMainOption extends NavigationDrawerOption {
+
+            int navigationDrawerOptionIcon;
+
+            NavigationDrawerMainOption(int position, int option, int navigationDrawerOptionIcon) {
+                super(position, option);
+                this.navigationDrawerOptionIcon = navigationDrawerOptionIcon;
+            }
+        }
+    }
+}
