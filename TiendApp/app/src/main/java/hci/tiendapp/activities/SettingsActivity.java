260fa674b226b7242b8535e9966c1504241b3dea
jmb <juanmbellini@hotmail.com>
2015-11-20 15:50:39 -0300

    SettingsFixes

--- TiendApp/app/src/main/java/hci/tiendapp/activities/SettingsActivity.java
+++ TiendApp/app/src/main/java/hci/tiendapp/activities/SettingsActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -31,7 +33,7 @@ public class SettingsActivity extends MyDrawerActivity implements View.OnClickLi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+
 
 
         this.txt_hello = (TextView) findViewById(R.id.txt_hello);
@@ -122,5 +124,23 @@ public class SettingsActivity extends MyDrawerActivity implements View.OnClickLi
                 break;
         }
         changeLang(lang);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.categories_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
