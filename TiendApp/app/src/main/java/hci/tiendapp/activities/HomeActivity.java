b510c4216aa80b07ac8b089d8412f1137c9c449f
safir93 <safir_93@live.se>
2015-11-23 20:56:06 -0300

    Onclicklistener buttons "See more" Home page

--- TiendApp/app/src/main/java/hci/tiendapp/activities/HomeActivity.java
+++ TiendApp/app/src/main/java/hci/tiendapp/activities/HomeActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 
 import hci.tiendapp.R;
@@ -12,9 +14,9 @@ import hci.tiendapp.R;
  * Created by JuanMarcos on 19/11/15.
  */
 
-public class HomeActivity extends MyDrawerActivity {
+public class HomeActivity extends MyDrawerActivity implements View.OnClickListener {
 
-
+    private Button btn_more_men, btn_more_women, btn_more_kids, btn_more_babies, btn_more_new_ins, btn_more_sale;
 
     public HomeActivity() {
         super(R.layout.activity_home, R.id.home_layout);
@@ -25,9 +27,20 @@ public class HomeActivity extends MyDrawerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        this.btn_more_men = (Button) findViewById(R.id.btn_more_men);
+        this.btn_more_women = (Button) findViewById(R.id.btn_more_women);
+        this.btn_more_kids = (Button) findViewById(R.id.btn_more_kids);
+        this.btn_more_babies = (Button) findViewById(R.id.btn_more_babies);
+        this.btn_more_new_ins = (Button) findViewById(R.id.btn_more_new_ins);
+        this.btn_more_sale = (Button) findViewById(R.id.btn_more_sale);
 
 
-
+        this.btn_more_men.setOnClickListener(this);
+        this.btn_more_women.setOnClickListener(this);
+        this.btn_more_kids.setOnClickListener(this);
+        this.btn_more_babies.setOnClickListener(this);
+        this.btn_more_new_ins.setOnClickListener(this);
+        this.btn_more_sale.setOnClickListener(this);
 
 
     }
@@ -45,5 +58,31 @@ public class HomeActivity extends MyDrawerActivity {
 
 
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_more_men:
+                //TODO: Show products in men etc.;
+                break;
+            case R.id.btn_more_women:
+                ;
+                break;
+            case R.id.btn_more_kids:
+                ;
+                break;
+            case R.id.btn_more_babies:
+                ;
+                break;
+            case R.id.btn_more_new_ins:
+                ;
+                break;
+            case R.id.btn_more_sale:
+                ;
+                break;
+            default:
+                break;
+        }
     }
 }
